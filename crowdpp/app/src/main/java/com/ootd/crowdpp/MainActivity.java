37ba47cedf25cdfeac91e70e70719b8db655b5a8
jihunchoi <geehoon62017@gmail.com>
2023-05-25 15:28:04 +0900

    retrofit 기본 파일 추가 및 activity 수정2

--- crowdpp/app/src/main/java/com/ootd/crowdpp/MainActivity.java
+++ crowdpp/app/src/main/java/com/ootd/crowdpp/MainActivity.java
@@ -3,12 +3,47 @@ package com.ootd.crowdpp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.ootd.crowdpp.Retrofits.DataModel;
+import com.ootd.crowdpp.Retrofits.RetrofitClient;
+
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    Call<DataModel> call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.Text1);
+
+        call = RetrofitClient.getApiService().test_my_server();
+
+        call.enqueue(new Callback<DataModel>(){
+            @Override
+            public void onResponse(Call<DataModel> call, Response<DataModel> response) {
+                if (response.code() == 200) {
+                    DataModel result = response.body();
+//                    textViewId.setText(result.getName());
+//                    textViewAutohr.setText(result.getCity());
+                    Log.e("TEST1", result.getName());
+                    textView.setText(result.getName());
+                    System.out.println(result);
+                } else {
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DataModel> call, Throwable t) {
+                Log.e("TEST2", t.getMessage());
+            }
+        });
     }
 }
