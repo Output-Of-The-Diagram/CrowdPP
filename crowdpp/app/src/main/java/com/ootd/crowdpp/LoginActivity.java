a146e1fe169e5779b233ad1aca5910d1f1f332be
kevinmj12 <kevinmj12@kookmin.ac.kr>
2023-05-29 19:44:40 +0900

    signup

--- crowdpp/app/src/main/java/com/ootd/crowdpp/LoginActivity.java
+++ crowdpp/app/src/main/java/com/ootd/crowdpp/LoginActivity.java
@@ -68,6 +68,9 @@ public class LoginActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+
                 String inputId = idInput.getText().toString();
                 String inputPw = pwInput.getText().toString();
 
