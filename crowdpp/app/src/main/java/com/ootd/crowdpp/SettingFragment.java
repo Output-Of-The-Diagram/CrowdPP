8c9428bcebec0fdedf98b08db4f84b0292245088
Seho-P <qkrtpgh77@gmail.com>
2023-06-16 23:02:28 +0900

    logout

--- crowdpp/app/src/main/java/com/ootd/crowdpp/SettingFragment.java
+++ crowdpp/app/src/main/java/com/ootd/crowdpp/SettingFragment.java
@@ -1,12 +1,33 @@
 package com.ootd.crowdpp;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.ootd.crowdpp.Retrofits.Result;
+import com.ootd.crowdpp.Retrofits.RetrofitClient;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +44,13 @@ public class SettingFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    int memberId;
+
+    Call<Result> call;
+
+    Context mContext;
+    ImageView logoutButton;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -53,12 +81,34 @@ public class SettingFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_setting, container, false);
+        // + 버튼을 누를 시 로그아웃
+        logoutButton = v.findViewById(R.id.logoutButton);
+        SharedPreferences shPreferences = requireContext().getSharedPreferences("userInfo", MODE_PRIVATE);
+
+
+        logoutButton.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "로그아웃 성공!", Toast.LENGTH_SHORT).show();
+                    SharedPreferences.Editor editor = shPreferences.edit();
+                    editor.clear();
+                    editor.commit();
+                    // 로그아웃 부분
+                    // 로그아웃이 되었다면 로그인 액티비티로 넘어간다.
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                            }
+        });
         return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 }
